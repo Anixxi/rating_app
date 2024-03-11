43dc2f58fc00c60e540d323444ba5b77e6d4b462
thry <thierry.ringlet@edu.ece.fr>
2024-03-11 23:02:13 +0100

    fixbug 1.0

--- back-skeleton/initdb/1_TABLES.sql
+++ back-skeleton/initdb/1_TABLES.sql
@@ -1,73 +1,30 @@
-CREATE TABLE Films (
-                       film_id SERIAL PRIMARY KEY,
-                       titre VARCHAR(255),
-                       réalisateur VARCHAR(255),
-                       année INT,
-                       synopsis TEXT,
-                       note_moyenne DECIMAL(3, 2),
-                       nombre_avis INT,
-                       catégorie_id INT,
-                       FOREIGN KEY (catégorie_id) REFERENCES Catégories(catégorie_id)
+create table students
+(
+    id SERIAL PRIMARY KEY,
+    first_name TEXT not null,
+    last_name TEXT not null,
+    birthdate date null,
+    major_id int null,
+    image bytea null
 );
 
-
-CREATE TABLE Restaurants (
-                             restaurant_id SERIAL PRIMARY KEY,
-                             nom VARCHAR(255),
-                             adresse VARCHAR(255),
-                             ville VARCHAR(255),
-                             code_postal VARCHAR(10),
-                             téléphone VARCHAR(15),
-                             note_moyenne DECIMAL(3, 2),
-                             nombre_avis INT,
-                             catégorie_id INT,
-                             FOREIGN KEY (catégorie_id) REFERENCES Catégories(catégorie_id)
+create table majors
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT not null,
+    description TEXT not null
 );
 
-
-CREATE TABLE Lieux (
-                       lieu_id SERIAL PRIMARY KEY,
-                       nom VARCHAR(255),
-                       type VARCHAR(255),
-                       adresse VARCHAR(255),
-                       ville VARCHAR(255),
-                       code_postal VARCHAR(10),
-                       téléphone VARCHAR(15),
-                       note_moyenne DECIMAL(3, 2),
-                       nombre_avis INT,
-                       catégorie_id INT,
-                       FOREIGN KEY (catégorie_id) REFERENCES Catégories(catégorie_id)
+create table courses
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT not null,
+    hours int not null
 );
 
-
-CREATE TABLE Catégories (
-                            catégorie_id SERIAL PRIMARY KEY,
-                            nom_catégorie VARCHAR(255)
+create table student_course
+(
+    id SERIAL PRIMARY KEY,
+    student_id int not null,
+    course_id int not null
 );
-
-CREATE TABLE Avis (
-                      avis_id SERIAL PRIMARY KEY,
-                      utilisateur_id INT,
-                      film_id INT,
-                      restaurant_id INT,
-                      lieu_id INT,
-                      note DECIMAL(3, 2),
-                      commentaire TEXT,
-                      date_avis DATE,
-                      FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(utilisateur_id),
-                      FOREIGN KEY (film_id) REFERENCES Films(film_id) ON DELETE CASCADE,
-                      FOREIGN KEY (restaurant_id) REFERENCES Restaurants(restaurant_id) ON DELETE CASCADE,
-                      FOREIGN KEY (lieu_id) REFERENCES Lieux(lieu_id) ON DELETE CASCADE
-);
-
-CREATE TABLE Utilisateurs (
-                              utilisateur_id SERIAL PRIMARY KEY,
-                              nom VARCHAR(255),
-                              prenom VARCHAR(255),
-                              email VARCHAR(255) UNIQUE,
-                              mot_de_passe VARCHAR(255),
-                              date_inscription DATE,
-                              type_utilisateur VARCHAR(20) DEFAULT 'utilisateur' CHECK (type_utilisateur IN ('utilisateur', 'admin'))
-);
-
-
--- front-skeleton/src/app/app-routing.module.ts
+++ front-skeleton/src/app/app-routing.module.ts
@@ -1,11 +1,45 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
 import { HomeComponent } from "home/home.component"
-import { CategorieComponent } from "categorie/categorie.component" // Import du composant de catégorie
+import { StudentsComponent } from "students/students.component"
+import { StudentsResolver } from "students/students.resolver"
+import { StudentDetailsComponent } from "students/student-details/student-details.component"
+import { StudentDetailsResolver } from "students/student-details/student-details.resolver"
+import { MajorsComponent } from "majors/majors.component"
+import { MajorsResolver } from "majors/majors.resolver"
+import { MajorStudentsResolver } from "majors/major-students/major-students.resolver"
+import { MajorStudentsComponent } from "majors/major-students/major-students.component"
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "catégorie", component: CategorieComponent }, // Ajout de la route pour les catégories
+  {
+    path: "etudiants",
+    component: StudentsComponent,
+    resolve: {
+      students: StudentsResolver,
+    },
+  },
+  {
+    path: "details-etudiant/:id",
+    component: StudentDetailsComponent,
+    resolve: {
+      student: StudentDetailsResolver,
+    },
+  },
+  {
+    path: "filieres",
+    component: MajorsComponent,
+    resolve: {
+      majors: MajorsResolver,
+    },
+  },
+  {
+    path: "etudiants-filiere/:id",
+    component: MajorStudentsComponent,
+    resolve: {
+      studentsFromMajor: MajorStudentsResolver,
+    },
+  },
 ]
 
 @NgModule({
